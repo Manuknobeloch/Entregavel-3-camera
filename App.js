76a0b54fca7084e4f7d5e8e726c7a3a6ceba2c2b
Manuela Knobeloch <knobelochmanuela@gmail.com>
2026-05-19 20:39:26 -0300

    filtros

--- App.js
+++ App.js
@@ -6,7 +6,8 @@ import AntDesign from "@expo/vector-icons/AntDesign";
 
 export default function App() {
   const [ultimaFoto, setUltimaFoto] = useState(null);
-  const cameraRef = useRef();
+  const [filtro, setFiltro] = useState("normal");
+  const cameraRef = useRef(null);
   const [permission, requestCameraPermission] = useCameraPermissions();
 
   async function quandoInicializa() {
@@ -42,7 +43,44 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <CameraView ref={cameraRef} style={styles.camera} facing="back" />
+      <View style={{ flex: 1, width: "100%", height: "100%" }}>
+  <CameraView
+  style={styles.camera}
+  facing="back"
+  ref={cameraRef}
+/>
+
+
+  {filtro === "vintage" && (
+    <View style={styles.vintageFilter} />
+  )}
+
+  {filtro === "neon" && (
+    <View style={styles.neonFilter} />
+  )}
+
+  {filtro === "dark" && (
+    <View style={styles.darkFilter} />
+  )}
+</View>
+
+<View style={styles.filtrosContainer}>
+  <TouchableOpacity onPress={() => setFiltro("normal")}>
+    <Text style={styles.filtroTexto}>Normal</Text>
+  </TouchableOpacity>
+
+  <TouchableOpacity onPress={() => setFiltro("vintage")}>
+    <Text style={styles.filtroTexto}>Vintage</Text>
+  </TouchableOpacity>
+
+  <TouchableOpacity onPress={() => setFiltro("neon")}>
+    <Text style={styles.filtroTexto}>Neon</Text>
+  </TouchableOpacity>
+
+  <TouchableOpacity onPress={() => setFiltro("dark")}>
+    <Text style={styles.filtroTexto}>Dark</Text>
+  </TouchableOpacity>
+</View>
       <TouchableOpacity
         style={styles.obturador}
         onPress={quandoPressionaObturador}
@@ -61,6 +99,44 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+
+  vintageFilter: {
+  position: "absolute",
+  width: "100%",
+  height: "100%",
+  backgroundColor: "rgba(120, 80, 40, 0.25)",
+},
+
+neonFilter: {
+  position: "absolute",
+  width: "100%",
+  height: "100%",
+  backgroundColor: "rgba(0, 120, 255, 0.15)",
+},
+
+darkFilter: {
+  position: "absolute",
+  width: "100%",
+  height: "100%",
+  backgroundColor: "rgba(0,0,0,0.35)",
+},
+filtrosContainer: {
+  position: "absolute",
+  bottom: 140,
+  width: "100%",
+  flexDirection: "row",
+  justifyContent: "space-evenly",
+  zIndex: 20,
+},
+
+filtroTexto: {
+  color: "white",
+  fontSize: 16,
+  backgroundColor: "rgba(0,0,0,0.5)",
+  paddingHorizontal: 12,
+  paddingVertical: 6,
+  borderRadius: 12,
+},
   camera: {
     width: "100%",
     height: "100%",
